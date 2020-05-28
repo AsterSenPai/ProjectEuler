c7e3fb2495e48a18976b7d551d7a41dfd3021697
AsterSenPai <kewalkris10@gmail.com>
2020-05-29 01:41:10 +0530

    First 2 problems

---
+++ script.js
@@ -0,0 +1,33 @@
+// Problem 3
+
+
+
+// Problem 2
+// let p = 1;
+// let n = 2;
+// let sum = 0;
+// var temp;
+
+// while(n <= 4000000) {
+//     if(n % 2 === 0) {
+//         sum += n;
+//     }
+//     temp = n;
+//     n = n + p;
+//     p = temp;
+// }
+
+// console.log(sum);
+
+
+// Problem 1
+
+// let sum = 0;
+
+// for(var i = 3; i < 1000; i++) {
+//     if(i % 3 == 0 || i % 5 == 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
